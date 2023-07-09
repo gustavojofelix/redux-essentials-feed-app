31450b858ba81af18ccea4f0bd0ba8969e92e138
Gustavo Felix <gustavojofelix@hotmail.com>
2023-07-09 06:50:20 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import { Fragment } from 'react'
+import { PostsList } from './features/posts/postsList'
+import { AddPostForm } from './features/posts/AddpostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Fragment>
+                <AddPostForm />
+                <PostsList />
+              </Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/features/posts/postsSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First Post!', content: 'Hello!' },
+  { id: '2', title: 'Second Post', content: 'More text' },
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+    // postUpdated(state, action) {
+    //   const { id, title, content } = action.payload
+    // },
+  },
+})
+
+export const { postAdded } = postsSlice.actions
+
+export default postsSlice.reducer
